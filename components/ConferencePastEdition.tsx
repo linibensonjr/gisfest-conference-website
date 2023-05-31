52f0461a77f497c515d119edf8465fe6abd4598a
Iniobong Benson <linibensonjr@gmail.com>
2023-05-31 20:38:44 +0100

    feat: Add past edition component

--- components/ConferencePastEdition.tsx
+++ components/ConferencePastEdition.tsx
@@ -1,8 +1,10 @@
-
-
 export default function ConferencePastEdition() {
         return (
             <>
+            <div className="flex grid-flow-row text-tc-0">
+            Be part of the conversation on the future of geospatial technology and discover how it can revolutionize the way we work, live, and interact with the world around us.
+            </div>
+
             </>
         
         )
